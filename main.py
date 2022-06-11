e028afa348a01b28da2195b4446aca3c47f69667
alexzitzewitz <alexanderzitzewitz@proofschool.org>
2022-06-10 23:30:54 -0700

    Finally, the code is working (mostly)

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import math
 import cmath
 from turtle import *
 import numpy as np
+import matplotlib.pyplot as plt
 
-radius = .02
+radius = .4
 time = 0
-speed = .0005
+speed = .00005
 deltax = complex(0,0)
 
 d2y = complex(0,0) # y''
@@ -14,22 +15,25 @@ y = complex(1,0)
 
 
 x = complex(radius,0)
-a = complex(3,0) #
+a = complex(-1,0) #
 gamma = complex(1/2,0)
 delta = complex(1/2,0)
 epsilon = complex(1/2,0)
 alpha = complex(1/4,0)
 beta = complex(1/4,0)
-B = complex(2, 0) # q = 4B
+B = complex(11,8) # q = B/4
 center = 0
-
 #turtle = Turtle()
+#screen = turtle.getscreen()
+#screen.tracer(20)
+
+bees = []
 
 #Common point that all paths go through:
 p = complex(1,1)
 def update_d2Y():
     global d2y
-    d2y = -(gamma/x+delta/(x-1)+epsilon/(x-a))*dy - ((alpha*beta*x-4*B)/(x*(x-1)*(x-a)))*y
+    d2y = -(gamma/x+delta/(x-1)+epsilon/(x-a))*dy - ((alpha*beta*x-B/4)/(x*(x-1)*(x-a)))*y
 
 def update_dY():
     global dy
@@ -44,13 +48,13 @@ def update_x():
     time = speed + time
     last_x = x
     if time < 1:
-        x += ((center + radius) - p) * speed
+        x = x + ((center + radius) - p) * speed
     elif time >= 1 and time < 2:
         x = radius * cmath.exp(time * complex(0, math.pi * 2)) + center
     elif time >= 2:
-        x += (p - (center + radius)) * speed
+        x = x + (p - (center + radius)) * speed
     deltax = x - last_x
-    #turtle.goto(x.real*100, x.imag*100)
+   # turtle.goto(x.real*100, x.imag*100)
 
 
 def update():
@@ -125,27 +129,56 @@ def findTraces(M0, M1, Ma):
 print('Done')
 
 
-def runpass(passes = 20, Bdelta = .01):
-    global B
+def runpass(passes = 50, Bdelta = .0001):
+    global B, bees
+    bees.append([B.real, B.imag])
+    #print("Matrices")
     B0 = B
     B1 = B + Bdelta
     B = B0
     Mset0 = findMatrices()
     B = B1
     Mset1 = findMatrices()
-    Tset0 = findTraces(Mset0[0],Mset0[1],Mset0[2])
-    Tset1 = findTraces(Mset1[0], Mset1[1], Mset1[2])
+    #print(str(Mset0[0]) + "\n" + str(Mset0[1]) + "\n" + str(Mset0[2]))
+    #print(str(Mset1[0]) + "\n" + str(Mset1[1]) + "\n" + str(Mset1[2]))
+    Tset0 = findTraces(Mset0[0],Mset0[1],Mset0[2]) # [t12(B0), t23(B0)]
+    Tset1 = findTraces(Mset1[0], Mset1[1], Mset1[2]) # [t12(B1), t23(B1)]
+    #print("Traces")
+    #print(Tset1[1])
+    #print(Tset0[1])
     dt12 = (Tset1[0] - Tset0[0])/(Bdelta)
     dt23 = (Tset1[1] - Tset0[1])/(Bdelta)
-    B = B0 + (dt23.conjugate()*Tset0[0].imag - dt12.conjugate()*Tset0[1].imag)/((dt12.conjugate()*dt23).imag)
+    b_x = (dt23.conjugate()*(Tset0[0].imag) - dt12.conjugate()*(Tset0[1].imag))/((dt12.conjugate()*dt23).imag)
+    #print(dt12)
+    #print(dt23)
+    #print("X:")
+    #print(b_x)
+    B = B0 + b_x*.3
+    print(B)
     passes -= 1
-    print("pass")
     if passes == 0:
         return Mset0
     else:
-        return runpass(passes)
+        return runpass(passes = passes)
+
+def makeEigenvalue(m, n):
+    return 1.43554 * (complex(m,n) ** 2) + 0.114237 * complex(m,n) / complex(m,-n)
+
+def findClosest(b):
+    closest = [None, None]
+    dist = float("inf")
+    for n in range(-10,11):
+        for m in range(-10, 11):
+            if (m,n) != (0,0) and abs(makeEigenvalue(m,n) - b) < dist:
+                closest = [m, n]
+                dist = abs(makeEigenvalue(m,n)-b)
+    return closest, dist
 results = runpass()
 print(results[0])
 print(results[1])
 print(results[2])
 print(B)
+print(findClosest(B))
+
+plt.plot(bees)
+plt.show()
