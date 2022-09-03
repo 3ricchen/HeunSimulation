1a37627ed6ac03e896e2f24f4cd179de3e00b1c7
zitzewitz <41396135+zitzewitz@users.noreply.github.com>
2022-09-03 10:39:47 -0700

    Updated the documentation and contents of main, EigenValueVisualization and VisualizationDatasetGenerator

--- VisualizationDatasetGenerator.py
+++ VisualizationDatasetGenerator.py
@@ -8,49 +8,37 @@
 
 from main import runpass
 
-# Gathering data: Stored in a 201x201 array
 
-#data = []
-#for real in range(-100, 101):
-#    print("row:" + str(real))
-#    row = []
-#    for imag in range(-100, 101):
-#        print("col:" + str(imag))
-#        row.append(findClosest(runpass(passes=10, setBstart=complex(real/10, imag/10), setSpeed=.01)[1]))
-#    data.append(row)
+def get_data(a, name_mod = "", passes=20, res=4, speed=.01, size=200):
+    '''
+    get_data
+    =================
+    Produces a file containing convergence data for a sizexsize square around the origin
 
-#data_out = open("eigenvaluedata4.csv", "w")
-#for row in data:
-#    for item in row[:-1]:
-#        data_out.write(str(item[0]))
-#        data_out.write(",")
-#    data_out.write(str(row[-1][0]))
-#    data_out.write("\n")
+    Parameters:
+    -----------
+    a: The value of a to be used in the Heun Equation
+    name_mod: An optional additional name modifier
+    passes: The number of recursive passes of main.runpass per pixel
+    res: pixels per unit in the resulting output dataset
+    speed: speed used in main.runpass calls
+    size: The number of pixels it extends from the origin in both directions
 
-#data_out.close()
-
-def get_data(a, name_mod = "", passes=20, res=4, speed=.01, size=200): # Produces a file named "eigenvaluedata_%a"
-    data_out = open("./Data/eigenvaluedata"+str(size)+"_"+name_mod+str(a)+".csv", "w")
+    Returns:
+    ----------
+    None
+    '''
+    data_out = open("./Data/eigenvaluedata"+str(size)+"_"+name_mod+str(a)+".csv", "w") # Opens the output file to write to
     for real in range(-size, size+1):
-        print(real)
-        #print("row:" + str(real))
+        print(real) # Helpful for knowing how far along the program is
         for imag in range(-size, size+1):
-            #print("col:" + str(imag))
-            eig = runpass(passes=passes, setBstart=complex(real / res, imag / res), setSpeed=speed)
+            # Change setEpsilon and setDelta as you wish to experiment with different Heun Equations
+            eig = runpass(passes=passes, setBstart=complex(real / res, imag / res), setSpeed=speed, setEpsilon=.625, setDelta=None, seta=a) # Computes the eigenvalue and writes it to the file
             data_out.write("[" + str(eig[1].real) + "," + str(eig[1].imag) + "]")
             if imag != size:
                 data_out.write(",")
         data_out.write("\n")
     data_out.close()
-
-#for a in range(-4,6): # Usually this is 5 and there is no if check, but my computer restarted so this was to keep already saved progress
-#    for b in range(-5,6):
-#        if a == -4 and b <= -4:
-#            pass
-#        else:
-#            print("setting a to " + str(complex(a,b)))
-#            get_data(complex(a,b))
-
 # Most recent normal computer run
 get_data(-1, name_mod="eigvalsH(.5,.5,.625)",res=4, size=200)
 
